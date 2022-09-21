efc7a3d3ee6d1d0089a85e86b3f30b07eb5f2790
mohammed <mohammedwanir67@gmail.com>
2022-09-21 14:58:45 +0100

    project version 1.0

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
-let cells = document.querySelectorAll('.box')
-cells = Array.from(cells)
-
 let currentPlayer = "X"
 
-let winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
 
-function checkWinner(){
-    winningCombinations.forEach(function(combination){
-        let check = combination.every(idx => cells[idx].innerText.trim() == currentPlayer)
-        if(check){
-            alert(currentPlayer+" won");
-                    location.reload();
-        }
-    })
+function startGame(){
+    fr=document.getElementById('first').value;
+    sc=document.getElementById('seconde').value;
+    window.localStorage.setItem('first',fr);
+    window.localStorage.setItem('seconde',sc);
+    document.getElementById("container1").classList.add("d-none");
+    document.getElementById("container2").classList.remove("d-none");
+    document.getElementById('firstn').innerText = window.localStorage['first']
+    document.getElementById('seconden').innerText = window.localStorage['seconde']
 }
-
-cells.forEach(function(cell){
-    cell.addEventListener('click', function(){
-        cell.innerText = currentPlayer
-        checkWinner()
+function clicked(id){
+      if(document.getElementById(id).innerText==""){  
+        document.getElementById(id).innerText = currentPlayer
+        window.localStorage.setItem(id,currentPlayer)
+        checkForWinner()
         currentPlayer = currentPlayer == "X" ? "O" : "X"
-    })
-}) 
+
+    }}
+
+    function checkForWinner(){
+        let a=window.localStorage.getItem('0');
+        let b=window.localStorage.getItem('1');
+        let c=window.localStorage.getItem('2');
+        let d=window.localStorage.getItem('3');
+        let e=window.localStorage.getItem('4');
+        let f=window.localStorage.getItem('5');
+        let g=window.localStorage.getItem('6');
+        let h=window.localStorage.getItem('7');
+        let i=window.localStorage.getItem('8');
+        if(a=="X" && b=="X" && c=="X" || d=="X" && e=="X" && f=="X" || g=="X" && h=="X" && i=="X" || a=="X" && d=="X" && g=="X" ||e=="X" && b=="X" && h=="X" || c=="X" && f=="X" && i=="X" || a=="X" && e=="X" && i=="X"||c=="X" && e=="X" && g=="X" )
+        {  
+            alert(currentPlayer+" winner");
+            restart();
+        }
+        else if(a=="O" && b=="O" && c=="O" || d=="O" && e=="O" && f=="O" || g=="O" && h=="O" && i=="O" || a=="O" && d=="O" && g=="O" ||e=="O" && b=="O" && h=="O" || c=="O" && f=="O" && i=="O" || a=="O" && e=="O" && i=="O"||c=="O" && e=="O" && g=="O" )
+        {
+         alert(currentPlayer+" winner");
+            restart();
+        }
+    }
+    
+function restart(){
+    window.localStorage.clear()
+    location.reload()
+
+    }
+
+
+ 
